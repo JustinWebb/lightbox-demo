d3a88d3ba05f0981c680fd1a9ea7c890e918269a
Justin Webb <jw@justinwebb.io>
2015-09-20 18:25:04 -0700

    (feat) Add AJAX request functionality to Flickr service

--- js/helpers.js
+++ js/helpers.js
@@ -33,7 +33,7 @@
 * @Author: justinwebb
 * @Date:   2015-09-20 15:24:21
 * @Last Modified by:   justinwebb
-* @Last Modified time: 2015-09-20 17:22:35
+* @Last Modified time: 2015-09-20 18:23:05
 */
 
 (function (window) {
@@ -44,14 +44,34 @@
   var _key = 'api_key=8fcaf784e87fdd001583cf05597a0945';
   var _format = 'format=json';
 
-  
+  var reportError = function (err) {
+    console.log('FlickrService:', err);
+  };
+
   var FlickrService = {
 
-    get: function (searchText) {
+    get: function (searchText, onSuccess, onFailure) {
       searchText = searchText || 'chocolate';
+      onFailure = onFailure || reportError;
       var args = [_method, _key, _format, 'text='+ searchText];
       var url = _api.concat(args.join('&'));
-      return url;
+
+      if (onSuccess === null) {
+        // print URL to the conosle for testing
+        console.log('FlickrService:', url);
+      } else {
+        // Make AJAX request to Flickr API
+        var request = new XMLHttpRequest();
+        request.onload = function () {
+          if (request.status >= 200 && request.status < 400) {
+            onSuccess(request.responseText);
+          }
+        };
+        request.onerror = onFailure;
+        request.open('GET', url, true);
+        request.send();
+      }
+
     }
   };
 
